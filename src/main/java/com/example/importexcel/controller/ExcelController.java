83e4560af23aee18ae93a756b8a7c8ecb40364a7
1710181854 <46155795+1710181854@users.noreply.github.com>
2019-04-02 15:02:55 +0800

    Update ExcelController.java

--- src/main/java/com/example/importexcel/controller/ExcelController.java
+++ src/main/java/com/example/importexcel/controller/ExcelController.java
@@ -43,8 +43,8 @@ private Insert is;
         Person p2 = new Person("1","2","3");
         personList.add(p1);
         personList.add(p2);
-        System.out.println("冲突3");
-        System.out.println("冲突4");
+        System.out.println("冲突31");
+        System.out.println("冲突41");
         ExcelUtiles.exportExcel(personList,"","1111",Person.class,"海贼王.xls",false,response);
         return  new JsonResult();
     }
